0f386d6c3f50e60febf7885f2758ce7df027c61b
Rob Landers <landers.robert@gmail.com>
2017-03-04 20:42:21 -0500

    Ready for initial publish

---
+++ index.js
@@ -0,0 +1,13 @@
+const Actor = require( './lib/Actor' );
+const Given = require( './lib/Given' );
+const MemStorage = require( './lib/MemStorage' );
+const RqlStorage = require( './lib/RqlStorage' );
+const Storage = require( './lib/Storage' );
+
+module.exports = {
+	Actor,
+	Given,
+	MemStorage,
+	RqlStorage,
+	Storage
+};
